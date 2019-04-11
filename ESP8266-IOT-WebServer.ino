620436b2c566fe6bdccaf3d2e7e0f6e42df33607
gulzarsuhail <suhail.95cse14@mietjammu.in>
2019-04-11 19:38:13 +0530

    added 404 route and page

--- ESP8266-IOT-WebServer.ino
+++ ESP8266-IOT-WebServer.ino
@@ -11,6 +11,8 @@
 #define STASSID "Zzzz"
 #define STAPSK  "qazwsxedc111"
 #endif
+const char* ssid = STASSID;
+const char* password = STAPSK;
 
 /*
  * The number of relays connected to nodemcu.
@@ -61,14 +63,11 @@ int relayPINSstatus[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 
 /*
- * Sets up an HTTP server at the PORT number provided as argument.
+ * Initialize HTTP server at the PORT number provided as argument.
  * If required, change PORT number here.
  * Default value: 80 (default HTTP PORT)
  */
 ESP8266WebServer server(80);
-
-const char* ssid = STASSID;
-const char* password = STAPSK;
 
 /*
  * Used to connect to WiFi.
@@ -91,6 +90,89 @@ void connectWiFi(){
   Serial.println(WiFi.localIP());
 }
 
+
+/* 
+ * Updates the current status of GPIO pins in the relayPINS
+ * into relayPINSstatus
+ */
+void readPINstatus(){
+  for (int x=0; x<RELAY_COUNT; x++)
+    relayPINSstatus[x] = digitalRead(relayPINS[x]);
+}
+
+/*
+ * Writes pin output based on values in relayPINSstatus
+ */
+void updatePINoputput(){
+  for (int x=0; x<RELAY_COUNT; x++)
+    digitalWrite(relayPINS[x], relayPINSstatus[x]);
+}
+
+/*
+ * Converts the status of the PINS into JSON which is later
+ * sent to the browser via AJAX
+ */
+String buildJSON(){
+  String res = "[";
+  for (int x=0; x<RELAY_COUNT; x++){
+    res += "{ 'deviceName':'";
+    res +=  deviceName[x];
+    res += "','pinNumber':";
+    res += relayPINS[x];
+    res += ", 'pinStatus':";
+    res += relayPINSstatus[x];
+    res += "},";
+  }
+  res += "]";
+  return res;
+}
+
+/*
+ * Send back HTTP page to the browser on GET request to "/".
+ * e.g, http://esp8266.local/
+ */
+void handleRoot(){
+  Serial.print("Recieved root GET request... ");
+  server.send(200, "text/plain", "Everything is A OK");
+  Serial.println("Response Sent");
+}
+
+/*
+ * Handels HTTP GET request to /status e.g, http://esp8266.local/status
+ * Send back JSON response with the current status of the relay modules
+ */
+void handleStatusRequest(){
+  Serial.println("Recieved /status GET request... ");
+  // update the status of the PINS
+  readPINstatus();
+  // build an JSON response
+  String resJSON = buildJSON();
+  Serial.println(resJSON);
+  server.send(200, "application/json", resJSON);
+  Serial.println("Response Sent");
+}
+
+
+void handleNotFound(){
+  Serial.println("Recieved GET request to undefined path... ");
+  // the html webpage for when path not found
+  String errorPageHTML="<!DOCTYPE html><html lang='en'><head> <meta charset='UTF-8'>\
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>\
+  <meta http-equiv='X-UA-Compatible' content='ie=edge'> <title>ESP8266 IOT ERROR 404</title>\
+  <style>html, body, #fullheight{min-height: 100% !important; height: 100%;\
+  padding: 0; margin: 0;}body{background: #000428; background: -webkit-linear-gradient\
+  (to right, #004e92, #000428); background: linear-gradient(to right, #004e92, #000428);\
+  color: white; display: flex; justify-content: center; align-items: center; flex-direction:\
+  column; font-family: Helvetica, Arial, sans-serif;}button{background-color: #4CAF50;\
+  border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none;\
+  display: inline-block; font-size: 16px; cursor: pointer;}</style></head><body>\
+  <h1 style='font-size:3.5em;'>4 0 4</h1> <h2 style='font-weight: 100; margin: 10px;'>\
+  The requested page cound not be found.</h2> <br><a href='/'><button>Go Home</button></a></body></html>";
+  // send response
+  server.send(404, "text/html", errorPageHTML);
+  Serial.println("Response Sent");
+}
+
 void setup() {
 
   // begin serial output for debug
@@ -98,11 +180,10 @@ void setup() {
 
   // set required pins to output
   pinMode(LED_BUILTIN, OUTPUT);
-  for (int x=0;x<RELAY_COUNT;x++) {
-    pinMode(relayPINS[x], OUTPUT);
-    // set the default PIN state to the relayPINSstatus declaration
-    digitalWrite(relayPINS[x], relayPINSstatus[x]);
-  }
+  for (int x=0;x<RELAY_COUNT;x++) pinMode(relayPINS[x], OUTPUT);
+
+  // update the default output of pins based on default values of relayPINSstatus
+  updatePINoputput();
 
   // set WiFi mode
   WiFi.mode(WIFI_STA);
@@ -110,9 +191,17 @@ void setup() {
   // connect to WiFi
   connectWiFi();
 
-  if (MDNS.begin("esp8266"))
-    Serial.println("MDNS responder started");
+  // mDNS, will make webpage available locally at http://esp8266.local
+  if (MDNS.begin("esp8266")) Serial.println("MDNS responder started");
   
+  // create routes for handeling HTTP requests
+  server.on("/", handleRoot);
+  server.on("/status", handleStatusRequest);
+  server.onNotFound(handleNotFound);
+  
+  // start the HTTP server
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
@@ -120,4 +209,6 @@ void loop() {
   // if WiFi is disconnected, try to reconnect
   if (WiFi.status() != WL_CONNECTED)  connectWiFi();
 
+  server.handleClient();
+  MDNS.update();
 }
